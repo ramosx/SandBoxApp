cf39853d27047822c4c1052d0eacc17020568666
ramosx <ron.amosa@gmail.com>
2015-04-06 00:46:12 +1200

    last save tonight. got display activity screen to display entered message.

--- app/src/main/java/sandboxapp/rxnamxsa/com/sandboxapp/DisplayMessageActivity.java
+++ app/src/main/java/sandboxapp/rxnamxsa/com/sandboxapp/DisplayMessageActivity.java
@@ -1,10 +1,12 @@
 package sandboxapp.rxnamxsa.com.sandboxapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -12,7 +14,19 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        //setContentView(R.layout.activity_display_message);
+
+        // get message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // get the text view at the activity layout
+        setContentView(textView);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
